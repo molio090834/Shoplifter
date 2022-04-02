7895ac8bc6dc4ad64bf2db403abcfffcc9b4be87
molio090834 <102860791+molio090834@users.noreply.github.com>
2022-04-02 15:13:23 +0300

    Update ModConfig.cs

--- Shoplifter/ModConfig.cs
+++ Shoplifter/ModConfig.cs
@@ -8,12 +8,12 @@ namespace Shoplifter
 {
     public class ModConfig
     {
-        public uint MaxShopliftsPerDay { get; set; } = 1;
-        public uint MaxShopliftsPerStore { get; set; } = 1;
+        public uint MaxShopliftsPerDay { get; set; } = 7;
+        public uint MaxShopliftsPerStore { get; set; } = 7;
         public uint MaxFine { get; set; } = 1000;
-        public uint FriendshipPenalty { get; set; } = 500;
+        public uint FriendshipPenalty { get; set; } = 0;
         public uint DaysBannedFor { get; set; } = 3;
         public uint CatchesBeforeBan { get; set; } = 3;
-        public uint CaughtRadius { get; set; } = 5;
+        public uint CaughtRadius { get; set; } = 7;
     }
 }
